713ad45da0861cc4ced19d3e56b985b9e3c4ab3a
Ernest Pazera <ernestpazera@gmail.com>
2024-06-21 17:24:14 -0500

    added all skill types

--- src/ParanoiaCharacterGenerator.Tests/Character_should.cs
+++ src/ParanoiaCharacterGenerator.Tests/Character_should.cs
@@ -6,6 +6,21 @@ public class Character_should
 {
     [Theory]
     [InlineData(SkillTypes.AlphaComplex)]
+    [InlineData(SkillTypes.Bureaucracy)]
+    [InlineData(SkillTypes.Psychology)]
+    [InlineData(SkillTypes.Science)]
+    [InlineData(SkillTypes.Bluff)]
+    [InlineData(SkillTypes.Charm)]
+    [InlineData(SkillTypes.Intimidate)]
+    [InlineData(SkillTypes.Stealth)]
+    [InlineData(SkillTypes.Demolition)]
+    [InlineData(SkillTypes.Engineer)]
+    [InlineData(SkillTypes.Operate)]
+    [InlineData(SkillTypes.Program)]
+    [InlineData(SkillTypes.Athletics)]
+    [InlineData(SkillTypes.Guns)]
+    [InlineData(SkillTypes.Melee)]
+    [InlineData(SkillTypes.Throw)]
     public void default_skill_to_zero(SkillTypes skillType)
     {
         ICharacter subject = new Character();
